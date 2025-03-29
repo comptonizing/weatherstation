a5d2d232610a1a8baf3b9d95bd778721760f1c0f
Philipp Weber <philipp.ph.weber@fau.de>
2025-03-29 23:49:54 +0100

    Add wind measurements

---
+++ firmware/Wind.cpp
@@ -0,0 +1,83 @@
+#include "Wind.h"
+
+Wind &Wind::i() {
+	static Wind theInstance;
+	return theInstance;
+}
+
+Wind::Wind() {
+	for (int ii=0; ii<WIND_HISTORY_SIZE; ii++) {
+		m_historyRaw[ii] = WIND_NAN;
+		m_historyTime[ii] = 0;
+	}
+}
+
+Wind::~Wind() {
+}
+
+int Wind::readRaw() {
+	return analogRead(WIND_PIN);
+}
+
+void Wind::update() {
+	mytime_t time = millis();
+
+	if ( (time - m_historyTime[0]) < WIND_SAMPLE_INTERVAL_SECONDS * 1000 ) {
+		// Not enough time passed, skip measurement
+		return;
+	}
+
+	// Shift everything in the buffer back
+	for (int ii=WIND_HISTORY_SIZE-1; ii>0; ii--) {
+		m_historyRaw[ii] = m_historyRaw[ii-1];
+		m_historyTime[ii] = m_historyTime[ii-1];
+	}
+
+	m_historyRaw[0] = readRaw();
+	m_historyTime[0] = time;
+}
+
+float Wind::avgRaw() {
+	mytime_t time = millis();
+	float avg = 0;
+	int n = 0;
+	for (int ii=0; ii<WIND_HISTORY_SIZE; ii++) {
+		if (
+				(time - m_historyTime[ii]) > WIND_PERIOD_SECONDS * 1000 ||
+				m_historyRaw[ii] == WIND_NAN
+		   ) {
+			break;
+		}
+		avg += m_historyRaw[ii];
+		n++;
+	}
+	avg /= n;
+	return avg;
+}
+
+int Wind::maxRaw() {
+	mytime_t time = millis();
+	int thismax = 0;
+	for (int ii=0; ii<WIND_HISTORY_SIZE; ii++) {
+		if (
+				(time - m_historyTime[ii]) > WIND_PERIOD_SECONDS * 1000 ||
+				m_historyRaw[ii] == WIND_NAN
+		   ) {
+			break;
+		}
+		if ( m_historyRaw[ii] > thismax ) {
+			thismax = m_historyRaw[ii];
+		}
+	}
+	return thismax;
+}
+
+float Wind::speed() {
+	update();
+	return RAW2SPEED(avgRaw());
+}
+
+float Wind::gust() {
+	update();
+	return RAW2SPEED(maxRaw());
+}
---
+++ firmware/Wind.h
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#include "common.h"
+
+#define WIND_PERIOD_SECONDS 600
+#define WIND_SAMPLE_INTERVAL_SECONDS 5
+#define WIND_HISTORY_SIZE WIND_PERIOD_SECONDS / WIND_SAMPLE_INTERVAL_SECONDS
+#define WIND_NAN -1
+#define WIND_PIN 0
+
+#define RAW2SPEED(raw) (raw / 1023. * 108.)
+
+class Wind {
+	public:
+		static Wind &i();
+    float avgRaw();
+		int maxRaw();
+		float speed();
+		float gust();
+		void update();
+
+	private:
+		Wind();
+		~Wind();
+		Wind(const Wind&);
+		Wind& operator=(const Wind&);
+
+		int readRaw();
+		int m_historyRaw[WIND_HISTORY_SIZE];
+		mytime_t m_historyTime[WIND_HISTORY_SIZE];
+};
