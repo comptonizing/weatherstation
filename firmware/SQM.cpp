24c20c16240c9992522cc500531a0c27d7e67526
Philipp Weber <philipp.ph.weber@fau.de>
2025-03-30 00:52:42 +0100

    Add SQM

---
+++ firmware/SQM.cpp
@@ -0,0 +1,57 @@
+#include "SQM.h"
+
+SQM &SQM::i() {
+	static SQM theInstance;
+	return theInstance;
+}
+
+SQM::SQM() {
+	m_tsl = new SQM_TSL2591(2591);
+	m_tsl->begin();
+	m_tsl->config.gain = TSL2591_GAIN_LOW;
+	m_tsl->config.time = TSL2591_INTEGRATIONTIME_200MS;
+	m_tsl->configSensor();
+	m_tsl->setCalibrationOffset(0.0);
+	m_tsl->verbose = false;
+	m_tsl->takeReading();
+}
+
+SQM::~SQM() {
+}
+
+uint16_t SQM::ir() {
+	return m_tsl->ir;
+}
+
+uint16_t SQM::full() {
+	return m_tsl->full;
+}
+
+uint16_t SQM::vis() {
+	return m_tsl->vis;
+}
+
+double SQM::mpsas() {
+	return m_tsl->mpsas;
+}
+
+double SQM::dmpsas() {
+	return m_tsl->dmpsas;
+}
+
+float SQM::integration() {
+	return m_tsl->integrationValue;
+}
+
+float SQM::gain() {
+	return m_tsl->gainValue;
+}
+
+void SQM::update() {
+	mytime_t time = millis();
+	if ( (time - m_lastReading) < SQM_SAMPLE_INTERVAL_SECONDS ) {
+		return;
+	}
+	m_tsl->takeReading();
+	m_lastReading = time;
+}
---
+++ firmware/SQM.h
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+
+#include "SQM_TSL2591.h"
+
+#include "common.h"
+
+#define SQM_SAMPLE_INTERVAL_SECONDS 5
+
+class SQM {
+	public:
+		static SQM &i();
+		void update();
+		uint16_t ir();
+		uint16_t full();
+		uint16_t vis();
+		double mpsas();
+		double dmpsas();
+		float integration();
+		float gain();
+	private:
+		SQM();
+		~SQM();
+		SQM(const SQM&);
+		SQM& operator=(const SQM&);
+
+		mytime_t m_lastReading = 0;
+		SQM_TSL2591 *m_tsl;
+};
