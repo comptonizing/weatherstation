159325ab159f561ee78c39bc271d51c82fc51712
Philipp Weber <philipp.ph.weber@fau.de>
2025-03-31 00:50:09 +0200

    Add rain sensor

---
+++ firmware/Rain.cpp
@@ -0,0 +1,129 @@
+#include "Rain.h"
+
+Rain &Rain::i() {
+	static Rain theInstance;
+	return theInstance;
+}
+
+Rain::Rain() :
+    m_pid(&m_lastTemperature, &m_pidDutyCycle, &m_targetTemperature, 4., 1., 1., DIRECT) {
+	m_pid.SetMode(AUTOMATIC);
+	analogWrite(RAIN_PIN_HEATING, 0);
+	pinMode(RAIN_PIN_CHARGE, OUTPUT);
+	update(true);
+}
+
+Rain::~Rain() {
+}
+
+float Rain::R2T(float R) {
+	float T0 = 87.4292636;
+	float R0 = 79.1795257;
+	float beta = 0.04098201;
+	return T0 - 1./beta * log(R / R0);
+}
+
+float Rain::readTemperature() {
+	int raw = analogRead(RAIN_PIN_TEMPERATURE);
+	float V = (raw * 5.) / 1024.;
+	float R = (V * RAIN_TEMPERATURE_RESISTANCE) / (12. - V);
+	return R2T(R);
+}
+
+float Rain::readCapacitance() {
+	mytime_t start, elapsed;
+	float capacitance;
+
+	// Discharge capacitor
+	digitalWrite(RAIN_PIN_CHARGE, LOW);
+	pinMode(RAIN_PIN_DISCHARGE, OUTPUT);
+	digitalWrite(RAIN_PIN_DISCHARGE, LOW);
+	start = micros();
+	while ( analogRead(RAIN_PIN_VOLTAGE) > 0 ) {
+		if ( (micros() - start) >= RAIN_TIMEOUT_MICROS ) {
+			return NAN;
+		}
+	}
+
+	// Charge and count time
+	pinMode(RAIN_PIN_DISCHARGE, INPUT);
+	start = micros();
+	digitalWrite(RAIN_PIN_CHARGE, HIGH);
+	while ( analogRead(RAIN_PIN_VOLTAGE) < RAIN_THRESHHOLD ) {
+		if ( (micros() - start) >= RAIN_TIMEOUT_MICROS ) {
+			digitalWrite(RAIN_PIN_CHARGE, LOW);
+			return NAN;
+		}
+	}
+	elapsed = micros() - start;
+
+	// Discharge capacitor
+	digitalWrite(RAIN_PIN_CHARGE, LOW);
+	pinMode(RAIN_PIN_DISCHARGE, OUTPUT);
+	digitalWrite(RAIN_PIN_DISCHARGE, LOW);
+	start = micros();
+	while ( analogRead(RAIN_PIN_VOLTAGE) > 0 ) {
+		if ( (micros() - start) >= RAIN_TIMEOUT_MICROS ) {
+			return NAN;
+		}
+	}
+
+	capacitance = ( (float) elapsed / RAIN_RESISTANCE ) * 1000 * 1000;
+	return capacitance;
+}
+
+float Rain::capacitance2percentage(float capacitance) {
+	if ( isnan(capacitance) ) {
+		return 100.;
+	}
+	if ( capacitance < RAIN_CAPACITANCE_DRY_PF ) {
+		return 0.0;
+	}
+	float val = 100. * 
+		(capacitance - RAIN_CAPACITANCE_DRY_PF) /
+		(RAIN_CAPACITANCE_WET_PF - RAIN_CAPACITANCE_DRY_PF);
+	return val > 100. ? 100. : val;
+}
+
+float Rain::heatingPower() {
+	update();
+	return 100. * m_pidDutyCycle / 255.;
+}
+
+void Rain::update(bool force) {
+	m_lastTemperature = readTemperature();
+	Serial.print("Temperature: ");
+	Serial.println(m_lastTemperature);
+	m_targetTemperature = BME280::i().temperature() + RAIN_TEMPERATURE_OFFSET;
+	Serial.print("Target: ");
+	Serial.println(m_targetTemperature);
+	m_pid.Compute();
+	Serial.print("Dutycycle: ");
+	Serial.println(m_pidDutyCycle);
+	analogWrite(RAIN_PIN_HEATING, m_pidDutyCycle);
+
+	mytime_t time = millis();
+	if ( (time - m_lastReading) < RAIN_SAMPLE_INTERVAL_SECONDS * 1000 && ! force ) {
+		return;
+	}
+
+	m_lastCapacitance = readCapacitance();
+	m_lastPercentage = capacitance2percentage(m_lastCapacitance);
+
+	m_lastReading = time;
+}
+
+float Rain::capacitance() {
+	update();
+	return m_lastCapacitance;
+}
+
+float Rain::percentage() {
+	update();
+	return m_lastPercentage;
+}
+
+float Rain::temperature() {
+	update();
+	return m_lastTemperature;
+}
---
+++ firmware/Rain.h
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <PID_v1.h>
+
+#include "common.h"
+#include "BME280.h"
+
+#define RAIN_SAMPLE_INTERVAL_SECONDS 5
+#define RAIN_PIN_CHARGE              7
+#define RAIN_PIN_DISCHARGE           8
+#define RAIN_PIN_VOLTAGE			 7
+#define RAIN_PIN_TEMPERATURE         1
+#define RAIN_PIN_HEATING             9
+#define RAIN_RESISTANCE              4.87e6
+#define RAIN_THRESHHOLD              648
+#define RAIN_TIMEOUT_MICROS          10000
+#define RAIN_TEMPERATURE_RESISTANCE  4700.
+#define RAIN_CAPACITANCE_DRY_PF      250.
+#define RAIN_CAPACITANCE_WET_PF      800.
+#define RAIN_TEMPERATURE_OFFSET      5
+
+class Rain {
+	public:
+		static Rain &i();
+		void update(bool force = false);
+		float capacitance();
+		float percentage();
+		float temperature();
+		float heatingPower();
+
+	private:
+		Rain();
+		~Rain();
+		Rain(const Rain&);
+		Rain& operator=(const Rain&);
+
+		float R2T(float R);
+		float readCapacitance();
+		float capacitance2percentage(float capacitance);
+		float readTemperature();
+
+		mytime_t m_lastReading = 0;
+		double m_lastTemperature = NAN;
+		float m_lastCapacitance = NAN;
+		float m_lastPercentage = NAN;
+		double m_pidDutyCycle = 0;
+		double m_targetTemperature = NAN;
+
+		PID m_pid;
+};
