bb16ae9e8cadfd3ae2ebd77436fa6f52a7aaa815
Philipp Weber <philipp.ph.weber@fau.de>
2025-03-31 00:49:57 +0200

    Add BME280

---
+++ firmware/BME280.cpp
@@ -0,0 +1,61 @@
+#include "BME280.h"
+
+BME280 &BME280::i() {
+	static BME280 theInstance;
+	return theInstance;
+}
+
+BME280::BME280() {
+	m_bme = new Adafruit_BME280();
+	m_bme->begin(BME280ADDR);
+	update(true);
+}
+
+BME280::~BME280() {
+}
+
+void BME280::update(bool force) {
+	mytime_t time = millis();
+	if ( (time - m_lastReading) < BME280_SAMPLE_INTERVAL_SECONDS * 1000 && ! force ) {
+		return;
+	}
+	m_lastReading = time;
+	Wire.beginTransmission(BME280ADDR);
+	if ( Wire.endTransmission() ) {
+		// Could not talk to BME
+		m_lastTemperature = NAN;
+		m_lastPressure = NAN;
+		m_lastHumidity = NAN;
+		m_lastDewpoint = NAN;
+		return;
+	}
+	m_lastTemperature = m_bme->readTemperature();
+	m_lastPressure = m_bme->readPressure() / 100.;
+	m_lastHumidity = m_bme->readHumidity();
+	if ( m_lastTemperature == NAN || m_lastHumidity == NAN ) {
+		m_lastDewpoint = NAN;
+		return;
+	}
+	float H = (log10(m_lastHumidity)-2)/0.4343 + (17.62*m_lastTemperature)/(243.12+m_lastTemperature);
+	m_lastDewpoint = 243.12*H/(17.62-H);
+}
+
+float BME280::temperature() {
+	update();
+	return m_lastTemperature;
+}
+
+float BME280::pressure() {
+	update();
+	return m_lastPressure;
+}
+
+float BME280::humidity() {
+	update();
+	return m_lastHumidity;
+}
+
+float BME280::dewpoint() {
+	update();
+	return m_lastDewpoint;
+}
---
+++ firmware/BME280.h
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+
+#include "common.h"
+
+#define BME280ADDR 0x76
+#define BME280_SAMPLE_INTERVAL_SECONDS 5
+
+class BME280 {
+	public:
+		static BME280 &i();
+		void update(bool force = false);
+		float temperature();
+		float pressure();
+		float humidity();
+		float dewpoint();
+	private:
+		BME280();
+		~BME280();
+		BME280(const BME280&);
+		BME280& operator=(const BME280&);
+
+		mytime_t m_lastReading = 0;
+		Adafruit_BME280 *m_bme;
+		float m_lastTemperature = NAN;
+		float m_lastPressure = NAN;
+		float m_lastHumidity = NAN;
+		float m_lastDewpoint = NAN;
+};
